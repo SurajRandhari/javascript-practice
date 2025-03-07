e911dced3c2849d6a2fb87383953977294bf2fd6
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-07 07:14:35 +0000

    vr practice

--- Variable/var.js
+++ Variable/var.js
@@ -1,1 +1,24 @@
 
+console.log('this is variable page');
+
+var a;
+a=100;
+
+console.log('a========>', a);
+
+let b;
+b=25;
+
+console.log('b========>', b);
+
+const c = 10;
+console.log('c========>',c);
+
+
+d = 75;
+
+a = d
+console.log('a========>', a);
+
+b = d
+console.log('b========>', b);
