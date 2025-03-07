e517d13211748a9ef289df2b7de0799b5864a819
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-07 10:38:52 +0000

    Str prac 1

---
+++ 02_String/str.js
@@ -0,0 +1,15 @@
+let text = "HELLO WORLD";
+
+let length = text.length;
+
+let char = text.charAt(2);
+
+let charCodeAt = text.charCodeAt(2);
+
+let at = text.at(2);
+
+
+console.log(length);
+console.log(char);
+console.log(charCodeAt);
+console.log(at);
