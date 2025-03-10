dcf7890dd504c8be3dae58dc68e99ccdfcdb40f8
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-10 05:29:48 +0000

    Scope in js

---
+++ Scope/ShadowScope.js
@@ -0,0 +1,10 @@
+function Shadow () {
+    let a = "Hello";
+    
+    if (true) {
+        let a = ""
+    }
+    console.log(a);
+
+    
+}
---
+++ Scope/scope.js
@@ -0,0 +1,15 @@
+ //Global scope
+ var a = 10;
+ //  console.log(a);
+
+ 
+ //Function Scope
+ function name() {
+    //  console.log(a);
+ }
+ name()
+
+ //Block Scope
+{
+     console.log(a);
+}
