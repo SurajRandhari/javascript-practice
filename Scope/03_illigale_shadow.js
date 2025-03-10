60f64251e1f6ea9e4e3512f00793bdd1bc76f74a
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-10 05:46:52 +0000

    illigale  shadow

---
+++ Scope/03_illigale_shadow.js
@@ -0,0 +1,15 @@
+function name() {
+    var a = "Hellow";
+    let b ="bye"
+
+    if (true) {
+        let a = "hii";
+        // var b = "Goodbye"
+
+        console.log(a);
+        console.log(b);
+        
+    }
+}
+
+name()
