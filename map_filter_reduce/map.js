ef53f0781266401b01114dc33b8e4a6520e33515
Suraj Randhari <surajrandhari@gmail.com>
2025-03-10 15:04:42 +0530

    Polyfill for map

--- map_filter_reduce/map.js
+++ map_filter_reduce/map.js
@@ -4,3 +4,17 @@ const mapArray = nums.map((num) => num + 2)
 console.log(mapArray);
 
 
+//!----------- Polyfill for map ---------------
+
+// Array.map((num, i , arr) => {})
+
+Array.prototype.myMap = function (cb) {
+    temp = []
+    for(let i = 0; i < this.length; i++) {
+        temp.push(cb(this[i]))
+    }
+    return temp
+}
+
+const myMapArray = nums.myMap((num) => num + 2)
+console.log(myMapArray);
