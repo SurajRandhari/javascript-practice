9144128e6f7c71fc60a8d6ecbb59935ab2c7db69
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-10 06:52:59 +0000

    Note: here  b & c (let & const) are not hoisted like a (var) but they are hoisted on Temporal Dead Zone

--- Hoisting/01_hoisting.js
+++ Hoisting/01_hoisting.js
@@ -23,8 +23,11 @@ function temporalDeadzone(){
     console.log(a,b,c);
 
     const c = 10;
-    let b = 10;
-    var a = 10;
+    let b = 10;  //Cannot access 'b' before initialization
+    var a = 10;  //Cannot access 'b' before initialization
 }
 
 temporalDeadzone()
+
+// Note: here  b & c (let & const) are not hoisted like a (var) but they are hoisted on Temporal Dead Zone
+//Temporal Dead Zone -  where variables are in the scops but they have been not declares yet. 
