0099e33fb3ac9ffc795ff0ac38bd78df220f2232
Suraj Randhari <110189854+SurajRandhari@users.noreply.github.com>
2025-03-10 05:54:39 +0000

    declaration

--- Scope/04_declare.js
+++ Scope/04_declare.js
@@ -1,1 +1,31 @@
+// ------------------------var-------------------------------------------
+// var can re-declare
+var a;
+var a;
+
+console.log(a);
+
+// ------------------------let-------------------------------------------
+// let cannot re-declare
+let b;
+// let b; //Cannot redeclare block-scoped variable 'b'
+
+console.log(b);.
+
+
+let ba; 
+{
+    let ba;    // it is correct because variable shadow
+}
+
+// ------------------------const-------------------------------------------
+//const also canont re-declare
+
+// const c; //also 'const' declarations must be initialized
+
+const c= 10;
+// const c= 10;
+
+console.log(c);
+
 
