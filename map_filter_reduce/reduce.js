f6cbe64aff7cef816e9f86c845c97e14def10d1e
Suraj Randhari <surajrandhari@gmail.com>
2025-03-10 15:55:28 +0530

    Polyfill for Reduce

--- map_filter_reduce/reduce.js
+++ map_filter_reduce/reduce.js
@@ -6,4 +6,21 @@ const reduceArray = nums.reduce((acc, cur) => {
 }, 0) // 0 is initials values
 console.log(reduceArray);
 
+//!----------- Polyfill for Reduce ---------------
+
+// nums.reduce((acc, cur, i, arr) => {}, initialValue)
+
+Array.prototype.MyReduce = function (cb, initialValue) {
+    var accumulator = initialValue;
+    for (let i = 0; i < this.length; i++) {
+        accumulator = accumulator ? cb(accumulator, this[i], i, this):this[i];
+    }
+    return accumulator;
+
+}
+
+const myReduceArray = nums.MyReduce((acc, cur) => {
+    return acc + cur
+}, 0) // 0 is initials values
+console.log(myReduceArray);
 
