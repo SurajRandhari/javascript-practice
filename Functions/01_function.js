f0524d6058200004cb583922674e0c446cddfa14
Suraj Randhari <surajrandhari@gmail.com>
2025-03-11 17:33:45 +0530

    first Class functions

--- Functions/01_function.js
+++ Functions/01_function.js
@@ -1,17 +1,29 @@
-//function Declaration
+// ----------- function Declaration -----------
 
 function addFn(num) {
     console.log(num * num);
 }
-
 addFn(10)
 
 
-//Function Expression - When you store a function inside a variable it is a called function variable
+//----------- Function Expression -----------
+// When you store a function inside a variable it is a called function Expression
 
 const expFn = (num) => {
     console.log(num*num);
- 
+}
+expFn(5)
+
+//----------- first Class functions -----------
+// function that are treated like a variable, like cases- functions can pass into another functions can be used , manupulated and return from those function , 
+//like- whatever a variable can do funvtion can also do called first class function
+
+function sqare(num) {
+  return  num * num
 }
 
-expFn(5)
+function displayFunction(fn) {
+    console.log("Square is " + fn(5));
+}
+
+displayFunction(sqare)
