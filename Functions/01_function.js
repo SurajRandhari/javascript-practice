d84769c95b272f8b5290ec9d31e33861adc28f40
Suraj Randhari <surajrandhari@gmail.com>
2025-03-11 17:43:12 +0530

    IIFE

--- Functions/01_function.js
+++ Functions/01_function.js
@@ -3,27 +3,38 @@
 function addFn(num) {
     console.log(num * num);
 }
-addFn(10)
+// addFn(10)
 
 
 //----------- Function Expression -----------
 // When you store a function inside a variable it is a called function Expression
 
 const expFn = (num) => {
-    console.log(num*num);
+    console.log(num * num);
 }
-expFn(5)
+// expFn(5)
 
 //----------- first Class functions -----------
 // function that are treated like a variable, like cases- functions can pass into another functions can be used , manupulated and return from those function , 
 //like- whatever a variable can do funvtion can also do called first class function
 
 function sqare(num) {
-  return  num * num
+    return num * num
 }
 
 function displayFunction(fn) {
     console.log("Square is " + fn(5));
 }
 
-displayFunction(sqare)
+// displayFunction(sqare)
+
+//----------- IIFE -----------
+
+// function iifeFn(num) {
+//     console.log(num * num);
+// }
+// iifeFn(2)
+
+(function iife(num) {
+    console.log(num * num);
+})(5);
