7cb46000a9091b55de111ea63f1ece6cb0eaea1a
Suraj Randhari <surajrandhari@gmail.com>
2025-03-11 17:23:01 +0530

    Function In Javascript

---
+++ Functions/01_function.js
@@ -0,0 +1,17 @@
+//function Declaration
+
+function addFn(num) {
+    console.log(num * num);
+}
+
+addFn(10)
+
+
+//Function Expression - When you store a function inside a variable it is a called function variable
+
+const expFn = (num) => {
+    console.log(num*num);
+ 
+}
+
+expFn(5)
